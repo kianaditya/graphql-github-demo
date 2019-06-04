e4acd20bd1ac8f20b6bfb2506784964dde268bd6
Aditya Naik <naik.aditya@gmail.com>
2019-06-04 20:27:37 +0200

    Updates App and MyProfile components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+import {ApolloClient} from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import MyProfile from './components/MyProfile'
+import { setContext } from 'apollo-link-context'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-boost'
+
+
+const httpLink = new HttpLink({ uri: 'https://api.github.com/graphql' })
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${
+        process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+      }`
+    }
+  }
+})
+
+const link = authLink.concat(httpLink)
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+})
+
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <h1>GitHub API</h1>
+        <MyProfile />
+      </div>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
